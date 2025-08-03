f80766d7a4bda846cbe5e09a6b23e485d77b082a
gile987 <mladen.reljic87@gmail.com>
2025-08-03 16:21:07 +0200

    feat: restructure routing to include a layout component with sidebar navigation
    - Added LayoutComponent to serve as a wrapper for child routes.
    - Updated app.routes.ts to redirect to LayoutComponent for main routes.
    - Created layout.component.html and layout.component.scss for the layout structure and styling.
    - Implemented sidebar navigation links for Dashboard, Upload, Releases, Royalties, and Profile.
    - Added basic styles for layout and reset global styles in styles.css.
    - Removed unused Tailwind CSS configuration files and styles.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,42 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'login',
     loadComponent: () =>
-      import('./routes/login/login.component').then((m) => m.LoginComponent),
+      import('./routes/login/login.component').then((m) => m.LoginComponent)
   },
   {
-    path: 'dashboard',
-    loadComponent: () =>
-      import('./routes/dashboard/dashboard.component').then(
-        (m) => m.DashboardComponent
-      ),
-  },
-  {
-    path: 'upload',
-    loadComponent: () =>
-      import('./routes/upload/upload.component').then((m) => m.UploadComponent),
-  },
-  {
-    path: 'releases',
-    loadComponent: () =>
-      import('./routes/releases/releases.component').then(
-        (m) => m.ReleasesComponent
-      ),
-  },
-  {
-    path: 'profile',
-    loadComponent: () =>
-      import('./routes/profile/profile.component').then(
-        (m) => m.ProfileComponent
-      ),
-  },
-  {
-    path: 'royalties',
-    loadComponent: () =>
-      import('./routes/royalties/royalties.component').then(
-        (m) => m.RoyaltiesComponent
-      ),
-  },
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./routes/dashboard/dashboard.component').then((m) => m.DashboardComponent)
+      },
+      {
+        path: 'upload',
+        loadComponent: () =>
+          import('./routes/upload/upload.component').then((m) => m.UploadComponent)
+      },
+      {
+        path: 'releases',
+        loadComponent: () =>
+          import('./routes/releases/releases.component').then((m) => m.ReleasesComponent)
+      },
+      {
+        path: 'royalties',
+        loadComponent: () =>
+          import('./routes/royalties/royalties.component').then((m) => m.RoyaltiesComponent)
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./routes/profile/profile.component').then((m) => m.ProfileComponent)
+      }
+    ]
+  }
 ];
