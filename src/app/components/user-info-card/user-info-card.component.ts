638b7873e5b2c43e9ef8edd1661589564fda0c99
gile987 <mladen.reljic87@gmail.com>
2025-08-05 16:21:54 +0200

    feat: add user info card component with styling and integrate into profile page

---
+++ src/app/components/user-info-card/user-info-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-info-card',
+  imports: [CommonModule],
+  templateUrl: './user-info-card.component.html',
+  styleUrls: ['./user-info-card.component.scss']
+})
+export class UserInfoCardComponent {
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) email!: string;
+  @Input() avatarUrl: string = 'https://www.gravatar.com/avatar/?d=mp';
+  @Input() role?: string;
+  @Input() joinedDate?: string;
+}
