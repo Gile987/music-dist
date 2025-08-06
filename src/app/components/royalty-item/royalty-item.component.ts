c8da29dce8a12e2dc9c1c8c2f2496a26dcf370ec
gile987 <mladen.reljic87@gmail.com>
2025-08-06 17:08:37 +0200

    feat: add RoyaltyItem component with metrics display and status badge

---
+++ src/app/components/royalty-item/royalty-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatusBadgeComponent } from '../status-badge/status-badge.component';
+import { Royalty } from '../../core/interfaces/royalty.interface';
+
+@Component({
+  selector: 'app-royalty-item',
+  imports: [CommonModule, StatusBadgeComponent],
+  templateUrl: './royalty-item.component.html',
+  styleUrls: ['./royalty-item.component.scss']
+})
+export class RoyaltyItemComponent {
+  @Input({ required: true }) royalty!: Royalty;
+}
