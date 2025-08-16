5f2c9065c8b51fefeed61d67ba27c807ee160b3f
gile987 <mladen.reljic87@gmail.com>
2025-08-16 16:17:17 +0200

    refactor: remove unused CommonModule imports from components

--- src/app/routes/landing/landing.component.ts
+++ src/app/routes/landing/landing.component.ts
@@ -1,13 +1,12 @@
 
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { StatCardComponent } from '../../components/stat-card/stat-card.component';
 import { ButtonComponent } from '../../shared/button.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [CommonModule, RouterLink, StatCardComponent, ButtonComponent],
+  imports: [RouterLink, StatCardComponent, ButtonComponent],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
