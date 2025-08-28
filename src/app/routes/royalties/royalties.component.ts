a292c19d899eed061be4e7332c07e306dc74b873
gile987 <mladen.reljic87@gmail.com>
2025-08-28 13:52:05 +0200

    feat: implement OnDestroy lifecycle hook and refactor royalty loading logic

--- src/app/routes/royalties/royalties.component.ts
+++ src/app/routes/royalties/royalties.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { RoyaltyListComponent } from '../../components/royalty-list/royalty-list.component';
 import { Royalty } from '../../core/interfaces/royalty.interface';
 import { Release } from '../../core/interfaces/release.interface';
 import { RoyaltyService } from '../../core/services/royalty.service';
 import { ReleaseService } from '../../core/services/release.service';
 import { AuthService } from '../../core/services/auth.service';
-import { forkJoin, firstValueFrom } from 'rxjs';
+import { forkJoin, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-royalties',
@@ -13,23 +14,37 @@ import { forkJoin, firstValueFrom } from 'rxjs';
   templateUrl: './royalties.component.html',
   styleUrls: ['./royalties.component.scss']
 })
-export class RoyaltiesComponent implements OnInit {
+export class RoyaltiesComponent implements OnInit, OnDestroy {
   royalties: Royalty[] = [];
   releases: Release[] = [];
   private readonly royaltyService: RoyaltyService = inject(RoyaltyService);
   private readonly releaseService: ReleaseService = inject(ReleaseService);
   private readonly authService: AuthService = inject(AuthService);
+  private readonly destroy$ = new Subject<void>();
 
   ngOnInit(): void {
-    this.authService.user$.subscribe(user => {
-      const artistId = user?.id ?? 0;
-      forkJoin([
-        this.royaltyService.getAllRoyalties(),
-        this.releaseService.getReleasesByArtist(artistId)
-      ]).subscribe(([royalties, releases]) => {
-        this.royalties = royalties;
-        this.releases = releases;
+    this.loadRoyaltiesAndReleases();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private loadRoyaltiesAndReleases(): void {
+    this.authService.user$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(user => {
+        const artistId = user?.id ?? 0;
+        forkJoin([
+          this.royaltyService.getAllRoyalties(),
+          this.releaseService.getReleasesByArtist(artistId)
+        ])
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(([royalties, releases]) => {
+          this.royalties = royalties;
+          this.releases = releases;
+        });
       });
-    });
   }
 }
